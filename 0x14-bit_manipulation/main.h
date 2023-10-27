2a9d1847a85dd7df916b94f553ae82864febf198
Sav-MI <saviour08100304152@gmail.com>
2023-10-27 10:58:21 +0000

    updated

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x13-bit_manipulation directory.
  */
+#include <stddef.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
